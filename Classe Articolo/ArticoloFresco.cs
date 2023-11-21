71d48ec93310f58ee626462bfbf03b3e52dcc77d
Zappa.20201 <Zappa.20201@E2-9-PC13>
2023-11-21 13:13:44 +0100

    Creazione classe ArticoloFresco

---
+++ Classe Articolo/ArticoloFresco.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_Articolo
+{
+	internal class Class1
+	{
+	}
+}
